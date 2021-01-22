bd4a1ea1eec26edd0323b2350254158337997308
kisei <kisei@kiseis-MBP.lan>
2021-01-22 11:10:54 -1000

    rcp ensemble spread plot added

--- scripts/data_curating/visualize_raw_RCP_nc_files.R
+++ scripts/data_curating/visualize_raw_RCP_nc_files.R
@@ -1,5 +1,5 @@
 
-setwd("/Users/ktanaka/clim_geo_disp/data")
+setwd("/Users/kisei/climate_geographic_disparity/data")
 
 library(raster)
 library(colorRamps)
@@ -14,6 +14,8 @@ rcp45_ensem_stdanom_1 = stack("CMIP5 ENSMN RCP4.5 ensemble stdanom (2006-2055)-(
 rcp45_ensem_stdanom_2 = stack("CMIP5 ENSMN RCP4.5 ensemble stdanom (2050-2099)-(1956-2005).nc", varname = "anomaly")
 rcp45_historical_stdanom_1 = stack("CMIP5 ENSMN RCP4.5 historical stdanom (2006-2055)-(1956-2005).nc", varname = "anomaly")
 rcp45_historical_stdanom_2 = stack("CMIP5 ENSMN RCP4.5 historical stdanom (2050-2099)-(1956-2005).nc", varname = "anomaly")
+rcp45_ensem_spread_1 = stack("CMIP5 ENSMN RCP4.5 ensemble spread (2006-2055)-(1956-2005).nc", varname = "histstddev")
+rcp45_ensem_spread_2 = stack("CMIP5 ENSMN RCP4.5 ensemble spread (2050-2099)-(1956-2005).nc", varname = "histstddev")
 
 rcp85_anom_1 = stack("CMIP5 ENSMN RCP8.5 anomaly (2006-2055)-(1956-2005).nc", varname = "anomaly")
 rcp85_anom_2 = stack("CMIP5 ENSMN RCP8.5 anomaly (2050-2099)-(1956-2005).nc", varname = "anomaly")
@@ -23,6 +25,8 @@ rcp85_ensem_stdanom_1 = stack("CMIP5 ENSMN RCP8.5 ensemble stdanom (2006-2055)-(
 rcp85_ensem_stdanom_2 = stack("CMIP5 ENSMN RCP8.5 ensemble stdanom (2050-2099)-(1956-2005).nc", varname = "anomaly")
 rcp85_historical_stdanom_1 = stack("CMIP5 ENSMN RCP8.5 historical stdanom (2006-2055)-(1956-2005).nc", varname = "anomaly")
 rcp85_historical_stdanom_2 = stack("CMIP5 ENSMN RCP8.5 historical stdanom (2050-2099)-(1956-2005).nc", varname = "anomaly")
+rcp85_ensem_spread_1 = stack("CMIP5 ENSMN RCP8.5 ensemble spread (2006-2055)-(1956-2005).nc", varname = "histstddev")
+rcp85_ensem_spread_2 = stack("CMIP5 ENSMN RCP8.5 ensemble spread (2050-2099)-(1956-2005).nc", varname = "histstddev")
 
 rcp_anom = stack(rcp45_anom_1, rcp45_anom_2,
                  rcp85_anom_1, rcp85_anom_2)
@@ -56,10 +60,18 @@ names(rcp_historical_stdanom) = c("RCP4.5_2006_2055",
                                   "RCP8.5_2006_2055",
                                   "RCP8.5_2050_2099")
 
+rcp_ensemble_spread = stack(rcp45_ensem_spread_1, rcp45_ensem_spread_2, 
+                            rcp85_ensem_spread_1, rcp85_ensem_spread_2)
+
+names(rcp_ensemble_spread) = c("RCP4.5_2006_2055", 
+                               "RCP4.5_2050_2099",
+                               "RCP8.5_2006_2055",
+                               "RCP8.5_2050_2099")
+
 pdf("~/Desktop/rcp_anom.pdf", width = 11, height = 6)
 par(mfrow = c(2,2), mar = c(2,3,3,3)
     # fg = 'white', col.axis = 'white', col.main="white", col.lab = 'white'
-    )
+)
 plot(raster::rotate(rcp_anom$RCP4.5_2006_2055), main = "RCP4.5 2006-2055", zlim = c(0,11), col = matlab.like(100), axes = F); map(add = T, lwd = 0.5); degAxis(1); degAxis(2, las = 1)
 plot(raster::rotate(rcp_anom$RCP4.5_2050_2099), main = "RCP4.5 2050-2099", zlim = c(0,11), col = matlab.like(100), axes = F); map(add = T, lwd = 0.5); degAxis(1); degAxis(2, las = 1)
 plot(raster::rotate(rcp_anom$RCP8.5_2006_2055), main = "RCP8.5 2006-2055", zlim = c(0,11), col = matlab.like(100), axes = F); map(add = T, lwd = 0.5); degAxis(1); degAxis(2, las = 1)
@@ -95,4 +107,14 @@ plot(raster::rotate(rcp_historical_stdanom$RCP4.5_2006_2055), main = "RCP4.5 200
 plot(raster::rotate(rcp_historical_stdanom$RCP4.5_2050_2099), main = "RCP4.5 2050-2099", zlim = c(0,14.9), col = matlab.like(100), axes = F); map(add = T, lwd = 0.5); degAxis(1); degAxis(2, las = 1)
 plot(raster::rotate(rcp_historical_stdanom$RCP8.5_2006_2055), main = "RCP8.5 2006-2055", zlim = c(0,14.9), col = matlab.like(100), axes = F); map(add = T, lwd = 0.5); degAxis(1); degAxis(2, las = 1)
 plot(raster::rotate(rcp_historical_stdanom$RCP8.5_2050_2099), main = "RCP8.5 2050-2099", zlim = c(0,14.9), col = matlab.like(100), axes = F); map(add = T, lwd = 0.5); degAxis(1); degAxis(2, las = 1)
+dev.off()
+
+pdf("~/Desktop/rcp_ensemble_spread.pdf", width = 11, height = 6)
+par(mfrow = c(2,2), mar = c(2,3,3,3)
+    # fg = 'white', col.axis = 'white', col.main="white", col.lab = 'white'
+)
+plot(raster::rotate(rcp_ensemble_spread$RCP4.5_2006_2055), main = "RCP4.5 2006-2055", zlim = c(0,3.37), col = matlab.like(100), axes = F); map(add = T, lwd = 0.5); degAxis(1); degAxis(2, las = 1)
+plot(raster::rotate(rcp_ensemble_spread$RCP4.5_2050_2099), main = "RCP4.5 2050-2099", zlim = c(0,3.37), col = matlab.like(100), axes = F); map(add = T, lwd = 0.5); degAxis(1); degAxis(2, las = 1)
+plot(raster::rotate(rcp_ensemble_spread$RCP8.5_2006_2055), main = "RCP8.5 2006-2055", zlim = c(0,3.37), col = matlab.like(100), axes = F); map(add = T, lwd = 0.5); degAxis(1); degAxis(2, las = 1)
+plot(raster::rotate(rcp_ensemble_spread$RCP8.5_2050_2099), main = "RCP8.5 2050-2099", zlim = c(0,3.37), col = matlab.like(100), axes = F); map(add = T, lwd = 0.5); degAxis(1); degAxis(2, las = 1)
 dev.off()
